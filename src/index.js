5e5d2bedc607d8da8bb2e1cb3f996d5bb25c7c7c
Arina Marchenko <arinamar7705@gmail.com>
2020-10-04 14:17:01 +0300

    feat: solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let length = str.length;
+    let comparer = bracketsConfig.map((item) => item.join(""));
+
+    for (let i = 0; i <= length; i++) {
+        comparer.forEach((item) => {
+            if (str.includes(item)) {
+                str = str.replace(item, "");
+            }
+        });
+    }
+
+    return str === "";
+};
